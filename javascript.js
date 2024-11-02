d36cfdd3157bceaaa95a1a4d0864d0bfc59fdfec
Ola <bancolejeandedieu@gmail.com>
2024-11-02 12:11:54 +0100

    fix: adding cleargrid and remove grid

--- javascript.js
+++ javascript.js
@@ -6,7 +6,8 @@ function createDivs(size=16){
         for (let j = 0; j < sizeX; j++)
         {
             let aDiv = document.createElement("div");
-            aDiv.setAttribute("class", "grid")
+            aDiv.setAttribute("class", "grid");
+            aDiv.style.border = "1px solid white";
             
             aDiv.style.width = `${divSize}px`;
             aDiv.style.height = `${divSize}px`;
@@ -17,7 +18,7 @@ function createDivs(size=16){
 }
 function removeDivs()
 {
-    childDiv = container.lastElementChild;
+    let childDiv = container.lastElementChild;
     while(childDiv)
     {
         container.removeChild(childDiv);
@@ -30,24 +31,45 @@ function randomNumber(max)
     return choice;
 }
 var container = document.querySelector("#container");
-function main()
-{
-    const range = document.querySelector("#sizeGrid");
-    document.querySelector("#printSize").textContent = `${range.value}`
-    createDivs(range.value);
-    containerChild = document.querySelectorAll(".grid")
-    range.addEventListener('change', () =>{
-        document.querySelector("#printSize").textContent = `${range.value}`;
-        removeDivs();
-        createDivs(range.value);
-    });
 
+function addHover()
+{
+    const containerChild = document.querySelectorAll(".grid")
     containerChild.forEach(element => {
         element.addEventListener("mouseover", () => {
             element.style.backgroundColor = `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)})`;
         })
         
     });
+    console.log("Hover applied")
+
+}
+
+function clearGrid()
+{
+    containerChild = document.querySelectorAll(".grid");
+    containerChild.forEach(element => {
+        element.style.backgroundColor = "white"        
+    });
+}
+function main()
+{
+    const range = document.querySelector("#sizeGrid");
+    document.querySelector("#printSize").textContent = `${range.value}`
+    createDivs(range.value);
+    addHover();
+    range.addEventListener('change', () =>{
+        document.querySelector("#printSize").textContent = `${range.value}`;
+        removeDivs();
+        createDivs(range.value);
+        addHover();
+    });
+    const rmDiv = document.querySelector("#rmGrid");
+    rmDiv.addEventListener("click", removeDivs)
+
+    const clrGrid = document.querySelector("#clearGrid");
+    clrGrid.addEventListener("click", clearGrid)
+    
 }
 
 main();
